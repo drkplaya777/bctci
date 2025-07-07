cff3595f44b82f444c6352457c977552060cdfca
Jeremy Walker <walkej19@pm.me>
2025-07-07 10:30:04 -0400

    feat: missing numbers in range
    The correct approach which treats the problem as two arrays you wanna see if the values exist in
    one but not the other.

--- Two Pointers/missing_numbers_in_range.py
+++ Two Pointers/missing_numbers_in_range.py
@@ -1,21 +1,26 @@
-def missing_in_range(arr, low, high):
-    if not arr:
-        return list(range(low, high + 1))
+def missing_numbers(array, low, high):
+    p1 = 0
+    p2 = low
     result = []
-    left, right = 0, len(arr) - 1
-    while arr[left] < low or arr[right] > high:
-        if left < len(arr) and arr[left] < low:
-            left += 1
-        if right > 0 and arr[right] > high:
-            right -= 1
 
-    for number in range(low, high + 1):
-        if number != arr[left] and number != arr[right]:
-            result.append(number)
+    while p1 < len(array) and p2 <= high:
+        if array[p1] < p2:
+            p1 += 1
+        elif array[p1] == p2:
+            p1 += 1
+            p2 += 1
+        else:
+            result.append(p2)
+            p2 += 1
+
+    if p2 <= high:
+        result.extend(range(p2, high + 1))
 
     return result
 
 if __name__ == '__main__':
-    print(missing_in_range([6, 9, 12, 15, 18], 9, 13))
-    print(missing_in_range([], 9, 9))
-    print(missing_in_range([6, 7, 8, 9], 7, 8))
+    print(missing_numbers([6, 9, 12, 15, 18], 9, 13))
+    print(missing_numbers([], 9, 9))
+    print(missing_numbers([6, 7, 8, 9], 7, 8))
+    print(missing_numbers([], 1, 5))
+    print(missing_numbers([1, 2, 3, 4, 5], 1, 5))
